9d839d3d0d99b60802c3c499d0b304ee7ae5e230
temirhanb <temirhan023@gmail.com>
2025-08-13 19:39:22 +0500

    - cancel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,8 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import App from "./app/App.tsx";
-import "./app/index.scss";
+import App from "./App.tsx";
+import "./index.scss";
 
 const queryClient = new QueryClient();
 
