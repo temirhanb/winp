49febd6e98b978da9b1a4913388c82577b56be82
temirhanb <temirhan023@gmail.com>
2025-08-13 20:04:39 +0500

    - fix auth

--- src/shared/api/fetchFakeItem/index.ts
+++ src/shared/api/fetchFakeItem/index.ts
@@ -4,7 +4,12 @@ import {API_URL} from "../api";
 export const fetchFakeItem = async (pages: number = 10, filter: string, skipItems: number = 0) => {
   const {data: {result}} = await axios.get(API_URL +
     `/Stock?${"Take=" + pages}&${"filter=" + filter}&${"skip=" + skipItems}`,
-    {headers: {"Authorization": " Basic Y2FuZGlkYXRlOmNhbmRpZGF0ZTMyMQ=="}}
+    {
+      auth: {
+        username: "candidate",
+        password: "candidate321"
+      },
+    }
   );
   return result;
 };
